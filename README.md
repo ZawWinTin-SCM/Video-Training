1c95e7527b1c409ebb5abbfb56dfa9859a5de0f0
Zaw Win Tin <scm.zawwintin@gmail.com>
2023-07-04 14:54:18 +0630

    Develop Video List using Video Tag

---
+++ README.md
@@ -0,0 +1,12 @@
+# Video Testing
+
+## Installation
+
+```
+npm install
+```
+
+## Run
+```
+npm run dev
+```
---
+++ src/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    window.HELP_IMPROVE_VIDEOJS = false;
+
+    let thumbnailVideos = document.querySelectorAll('.js-thumbnail-video');
+    let videoPlayer = document.querySelector('.js-video-player');
+    let videoPlayerSource = document.querySelector('.js-video-player-source');
+    let videoPlayerBackground = document.querySelector('.js-video-player-background');
+
+    thumbnailVideos.forEach((thumbnail, index) => {
+        thumbnail.addEventListener('click', () => {
+            let videoSrc = thumbnail.getAttribute('src');
+            videoPlayerSource.setAttribute('src', videoSrc);
+            videoPlayerBackground.classList.remove('hidden');
+            videoPlayerBackground.classList.add('flex');
+            videoPlayer.load();
+            videoPlayer.play();
+            thumbnail.pause();
+            thumbnail.currentTime = 0;
+        });
+
+        thumbnail.addEventListener('mouseenter', () => {
+            thumbnail.play();
+        });
+
+        thumbnail.addEventListener('mouseleave', () => {
+            thumbnail.pause();
+            thumbnail.currentTime = 0;
+        });
+    });
+
+    videoPlayerBackground.addEventListener('click', event => {
+        if (event.target === videoPlayerBackground) {
+            videoPlayerBackground.classList.remove('flex');
+            videoPlayerBackground.classList.add('hidden');
+            videoPlayer.pause();
+            videoPlayerSource.setAttribute('src', '');
+        }
+    });
+});
